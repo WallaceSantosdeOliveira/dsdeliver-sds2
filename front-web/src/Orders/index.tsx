81fe0bb20578d52dc91ec19b9c627ac746defe66
Wallace SantosdeOliveira <wallacesantos447@gmail.com>
2023-01-04 11:17:43 -0300

    Subindo

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,10 +1,28 @@
-import StepsHeader from './StepsHeader';
+import { useEffect, useState } from 'react';
+import { fetchProducts } from '../api';
+import OrderLocation from './OrderLocation';
+import ProductsList from './ProductsList';
+import { Product } from './types';
+import StepsHeader from './StepsHeader'; 
+
 import './styles.css';
 
+
 function Orders() {
+  const [products, setProducts] = useState<Product[]>([]);
+  
+
+useEffect(() => {
+  fetchProducts()
+  .then(response => setProducts(response.data))
+  .catch(error => console.log(error))
+}, []);
+
   return (
     <div className="orders-container">
        <StepsHeader />
+       <ProductsList products={products} />
+       <OrderLocation />
     </div>
   ) 
 }
